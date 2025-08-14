37b8514fe49fe8dd425983dea389e061d36c1f0b
Suvam Naskar <suvamnaskar780@gmail.com>
2025-08-14 14:26:23 +0530

    refactor: restructure FastAPI application to include API router and user endpoints

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
+from app.api.api_v1.api import router as api_router
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/users")
-async def get_users():
-    return {"message": "Get Users!"}
+app.include_router(api_router, prefix="/api/v1")
